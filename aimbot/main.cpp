b679e6aa6f5e552c140c2a44e11935428dcd723c
Lorenzo Tacca Orssatto <154294191+SenhorLo@users.noreply.github.com>
2024-10-16 07:14:49 -0300

    Add files via upload

---
+++ aimbot/main.cpp
@@ -0,0 +1,131 @@
+#include "memorie.h"
+#include "vector.h"
+
+#include <thread>
+
+
+
+namespace offset
+{
+	// client
+	constexpr ::std::ptrdiff_t dwLocalPlayer = 0xDA746C;
+	constexpr ::std::ptrdiff_t dwEntidyList = 0x4DC178;
+
+	// engine
+
+	constexpr ::std::ptrdiff_t dwClientState = 0x589FCC;
+	constexpr ::std::ptrdiff_t dwClientState_ViewAngles = 0x4D90;
+
+	// entity
+
+	constexpr ::std::ptrdiff_t m_dwBoneMatrix = 0x26A8;
+	constexpr ::std::ptrdiff_t m_bDortmant = 0xED;
+	constexpr ::std::ptrdiff_t m_iTeamNum = 0xF4;
+	constexpr ::std::ptrdiff_t m_iHealth = 0x100;
+	constexpr ::std::ptrdiff_t m_vecOrigin = 0x138;
+	constexpr ::std::ptrdiff_t m_vecViewOffset = 0x108;
+	constexpr ::std::ptrdiff_t m_aimPunchAngle = 0x303C;
+	constexpr ::std::ptrdiff_t m_bSpottedByMask = 0x980;
+
+}
+
+constexpr Vector3 CalculateAngle(
+	const Vector3& localPosition,
+	const Vector3& enemyPosition,
+	const Vector3& viewAngles) noexcept
+{
+	return ((enemyPosition - localPosition).ToAngle() - viewAngles);
+}
+
+
+
+
+int main()
+{
+
+	// initialize memory class
+
+	const auto memory = Memory{ "csgo.exe" };
+
+	// module addresses
+
+	const auto client = memory.GetModuleAddress("client.dll");
+	const auto engine = memory.GetModuleAddress("engine.dll");
+
+	// infinite hack loop
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+		// aimbot key
+		if (!GetAsyncKeyState(VK_RBUTTON))
+			continue;
+
+		// get local player 
+		const auto& localPlayer = memory.Read<std::uintptr_t>(client + offset::dwLocalPlayer);
+		const auto& localTeam = memory.Read<std::int32_t>(localPlayer + offset::m_iTeamNum);
+
+		// eye position = origin + viewOffset
+		const auto localEyePosition = memory.Read<Vector3>(localPlayer + offset::m_vecOrigin) + memory.Read<Vector3>(localPlayer + offset::m_vecViewOffset);
+
+		const auto& clientState = memory.Read<std::uintptr_t>(engine + offset::dwClientState);
+
+		const auto& viewAngles = memory.Read<Vector3>(clientState + offset::dwClientState_ViewAngles);
+		const auto& aimPunch = memory.Read<Vector3>(localPlayer + offset::m_aimPunchAngle) * 2;
+
+		// aimbot fov
+
+		auto bestFov = 5.f;
+		auto bestAngle = Vector3{ };
+
+		for (auto i = 1; i <= 32; ++i)
+		{
+			const auto& player = memory.Read<std::uintptr_t>(client + offset::dwEntidyList + i * 0x10);
+
+			// entity checks
+			if (memory.Read<std::int32_t>(player + offset::m_iTeamNum) == localTeam)
+				continue;
+
+			if (memory.Read<bool>(player + offset::m_bDortmant))
+				continue;
+
+			if (!memory.Read<std::int32_t>(player + offset::m_iHealth))
+				continue;
+
+			if (!memory.Read<bool>(player + offset::m_bSpottedByMask))
+				continue;
+
+			const auto boneMatrix = memory.Read < std::uintptr_t>(player + offset::m_dwBoneMatrix);
+
+			// position of player head in 3d space
+			
+			const auto playerHeadPosition = Vector3{
+				memory.Read<float>(boneMatrix + 0x30 * 8 + 0x0C),
+				memory.Read<float>(boneMatrix + 0x30 * 8 + 0x1C),
+				memory.Read<float>(boneMatrix + 0x30 * 8 + 0x2C)
+			};
+
+			const auto angle = CalculateAngle(
+				localEyePosition,
+				playerHeadPosition,
+				viewAngles + aimPunch
+			);
+
+			const auto fov = std::hypot(angle.x, angle.y);
+
+			if (fov < bestFov)
+			{
+				bestFov = fov;
+				bestAngle = angle;
+			}
+		}
+
+		// finely the aimbot!
+
+		if (!bestAngle.IsZero())
+			memory.Write<Vector3>(clientState + offset::dwClientState_ViewAngles, viewAngles + bestAngle / 3.f); // <- aimbot smooth 3.f
+
+	}
+
+	return 0;
+}
